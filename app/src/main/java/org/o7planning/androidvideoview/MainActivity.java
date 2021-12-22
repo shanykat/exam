5eedb969bc3344fcec43a58c5ba161dddcf3b631
shanykat <ekkadd27@gmail.com>
2021-12-22 14:21:35 +0300

    hi

---
+++ app/src/main/java/org/o7planning/androidvideoview/MainActivity.java
@@ -0,0 +1,106 @@
+package org.o7planning.androidvideoview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private VideoView videoView;
+    private int position = 0;
+    private MediaController mediaController;
+    private Button buttonRaw;
+    private Button buttonLocal;
+    private Button buttonURL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.videoView = (VideoView) findViewById(R.id.videoView);
+        this.buttonRaw = (Button) findViewById(R.id.button_raw);
+        this.buttonLocal = (Button) findViewById(R.id.button_local );
+        this.buttonURL = (Button) findViewById(R.id.button_url);
+
+        if (this.mediaController == null) {
+            this.mediaController = new MediaController(MainActivity.this);
+
+            // Set the videoView that acts as the anchor for the MediaController.
+            this.mediaController.setAnchorView(videoView);
+
+            // Set MediaController for VideoView
+            this.videoView.setMediaController(mediaController);
+        }
+        // When the video file ready for playback.
+        this.videoView.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+                videoView.seekTo(position);
+                if (position == 0) {
+                    videoView.start();
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the videoView that acts as the anchor for the MediaController
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
+        this.buttonRaw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String resName = VideoViewUtils.RAW_VIDEO_SAMPLE;
+                VideoViewUtils.playRawVideo(MainActivity.this, videoView, resName);
+            }
+        });
+
+        this.buttonLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String localPath = VideoViewUtils.LOCAL_VIDEO_SAMPLE;
+                VideoViewUtils.playLocalVideo(MainActivity.this, videoView, localPath);
+            }
+        });
+
+        this.buttonURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoURL = VideoViewUtils.URL_VIDEO_SAMPLE;
+                VideoViewUtils.playURLVideo(MainActivity.this, videoView, videoURL);
+            }
+        });
+    }
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
+    }
+
+    // After rotating the phone. This method is called.
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
+    }
+}
